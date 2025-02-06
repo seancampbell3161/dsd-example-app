471d8a52b834f383bfba3bb0c2780c83b6d3958a
Sean Campbell <sean.campbell3161@gmail.com>
2025-02-05 23:14:22 -0600

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PokemonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
