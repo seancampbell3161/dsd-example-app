653d5a2762a91a8f700a9ed134d9cc21f4778a73
Sean Campbell <sean.campbell3161@gmail.com>
2025-02-05 23:11:47 -0600

    add service mehtods

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LocationEncounter, Pokemon } from '../models/poke-api';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +10,13 @@ export class PokemonService {
 
   private apiUrl = ''
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  searchPokemon(name: string): Observable<Pokemon | null> {
+    return this.http.get<Pokemon | null>(`${this.apiUrl}pokemon/${name}`);
+  }
 
+  getLocationEncounters(id: number) {
+    return this.http.get<LocationEncounter | null>(`${this.apiUrl}pokemon/${id}/encounters`);
+  }
 }
