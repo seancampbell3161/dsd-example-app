5d9913c93281ecb3693b391616f4b66951c54470
Sean Campbell <sean.campbell3161@gmail.com>
2025-02-05 23:03:55 -0600

    init commit

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -12,34 +12,10 @@ import { PokemonService } from '../../services/pokemon.service';
 export class PokemonComponent {
   pokemonService = inject(PokemonService);
 
-  pokemon: Pokemon | null = null;
-  locationEncounters: LocationEncounter | null = null;
   pokemonSearch = new FormControl('');
 
+  pokemon = this.pokemonService.pokemon;
+  locationEncounters = this.pokemonService.encounters;
+
   constructor() {}
-
-  search() {
-    if (this.pokemonSearch === null) { return; }
-
-    this.pokemonService.searchPokemon(this.pokemonSearch.value!).subscribe({
-      next: (res) => this.handlePokemonResponse(res),
-      error: (e) => console.error(e),
-    });
-  }
-
-  private handlePokemonResponse(res: Pokemon | null) {
-    if (res) {
-      this.pokemon = res;
-      this.pokemonService.getLocationEncounters(res.id).subscribe({
-        next: (res) => this.handleEncountersResponse(res),
-        error: (e) => console.error(e)
-      })
-    }
-  }
-
-  private handleEncountersResponse(res: LocationEncounter | null) {
-    if (res) {
-      this.locationEncounters = res;
-    }
-  }
 }
