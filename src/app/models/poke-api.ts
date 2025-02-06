14ece5026251608be863266a9593d59e39dfa433
Sean Campbell <sean.campbell3161@gmail.com>
2025-02-05 23:10:16 -0600

    ufpdaf

--- src/app/models/poke-api.ts
+++ src/app/models/poke-api.ts
@@ -374,3 +374,44 @@ export interface Type2 {
   name: string
   url: string
 }
+
+export type LocationEncounter = Location[]
+
+export interface Location {
+  location_area: LocationArea
+  version_details: VersionDetail[]
+}
+
+export interface LocationArea {
+  name: string
+  url: string
+}
+
+export interface VersionDetail {
+  max_chance: number
+  encounter_details: EncounterDetail[]
+  version: Version
+}
+
+export interface EncounterDetail {
+  min_level: number
+  max_level: number
+  condition_values: ConditionValue[]
+  chance: number
+  method: Method
+}
+
+export interface ConditionValue {
+  name: string
+  url: string
+}
+
+export interface Method {
+  name: string
+  url: string
+}
+
+export interface Version {
+  name: string
+  url: string
+}
